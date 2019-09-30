05ed9f35477492e8a9fb924cf47522f73b3e6550
sergey.sh <greyson.dean@gmail.com>
2019-09-30 19:03:37 +0500

    wip - added navigation bar color changing for API >= 26 (Oreo, Android 8)

--- app/src/main/java/com/greylabsdev/pexwalls/presentation/screen/host/HostActivity.kt
+++ app/src/main/java/com/greylabsdev/pexwalls/presentation/screen/host/HostActivity.kt
@@ -2,14 +2,24 @@ package com.greylabsdev.pexwalls.presentation.screen.host
 
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import android.os.Bundle
 import com.greylabsdev.pexwalls.R
 import com.greylabsdev.pexwalls.presentation.base.BaseActivity
 import com.greylabsdev.pexwalls.presentation.base.BaseViewModel
+import com.greylabsdev.pexwalls.presentation.ext.then
 
 class HostActivity : BaseActivity(
     layoutResId = R.layout.activity_host
 ) {
     override val viewModel: BaseViewModel? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O).then {
+            window.navigationBarColor = getColor(R.color.colorBackground)
+        }
+    }
 
     companion object {
         fun start(context: Context) {
